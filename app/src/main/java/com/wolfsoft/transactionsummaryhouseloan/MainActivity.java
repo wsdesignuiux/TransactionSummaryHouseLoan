d722b39a8cb2dc757b43b9615a3c74e38930a05e
wolfsoft_design <info.wsdesignuiux@gmail.com>
2018-11-22 17:41:15 +0530

    Initial commit

---
+++ app/src/main/java/com/wolfsoft/transactionsummaryhouseloan/MainActivity.java
@@ -0,0 +1,20 @@
+package com.wolfsoft.transactionsummaryhouseloan;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
